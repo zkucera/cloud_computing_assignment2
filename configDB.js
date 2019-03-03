a645a043a48ec31557ace1e1b763c236ff21ba1d
ConnorSoulliere <connor.soulliere97@gmail.com>
2019-03-03 16:53:42 -0500

    login stuff

---
+++ configDB.js
@@ -0,0 +1,6 @@
+var crypto = require('crypto').randomBytes(256).toString('hex');
+
+module.exports = {
+    uri: 'mongodb://localhost:27017/cloudDB',
+    secret: crypto
+};
--- models/user.js
+++ models/user.js
@@ -1,10 +1,31 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
+var bcrypt       = require('bcryptjs');
 
-var User = new Schema({
-    username: String,
-    password: String
 
+var UserSchema = new Schema({
+    username: {type: String, required: true, unique: true, lowercase: true},
+    password: {type: String, required: true}
 });
 
-module.exports = mongoose.model('User', User);
+UserSchema.pre('save', function(next) {
+
+    // only hash the password if it has been modified (or is new)
+    if (!this.isModified('password')){
+        return next();
+    }
+
+    // generate a salt
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(this.password, salt);
+    this.password = hash;
+    next();
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compareSync(candidatePassword, this.password);
+};
+
+
+
+module.exports = mongoose.model('User', UserSchema);
