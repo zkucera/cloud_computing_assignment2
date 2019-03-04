b638fbd6362720b3211ba432fe8d850d4a0e357d
chamipon <zkucera@uwo.ca>
2019-03-03 20:11:17 -0500

    Register working, login working except for web token

--- public/register.js
+++ public/register.js
@@ -1,12 +1,21 @@
 var un,pw,pwc
+var url = 'http://localhost:8080/api/register';
 function RegisterSubmit(){
 
-  un = document.getElementById("un").value //Get the login info entered
+  un = document.getElementById("un").value //Get the info entered
   pw = document.getElementById("pw").value
   pwc = document.getElementById("pwc").value
 
-  if (pw == pwc){//if its the correct login info
-    location.assign("./")
+  if (pw == pwc){//if the passwords match
+    fetch(url, { //Send the register request
+      method: 'post',
+      headers: {'Content-Type': 'application/json' , 'Access-Control-Allow-Origin' : url},
+      body: JSON.stringify({username : un, password : pw})})
+      .then(x =>{
+        console.log("Registration Successful!: " + x.statusText)
+      }).catch(error =>{
+        console.log("ERROR! Register did not go through!: " + error)
+      })
   }
   else{//if its incorrect
     document.getElementById("pw").value = ""
